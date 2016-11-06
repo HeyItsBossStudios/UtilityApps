4849df681667d33a98203b9fc6873150f5e9fff1
HeyItsBossStudios <xenathedog2008@gmail.com>
2016-11-05 19:56:51 -0500

    Update Simple Calculator.cs

--- Simple Calculator.cs
+++ Simple Calculator.cs
@@ -11,14 +11,18 @@ namespace Simple_Calculator
 	{
 		static void Main(string[] args)
 		{
-			
+			//These string statements read the numbers that the users imput,
+			//val1 = 1st number, val2 = 2nd number, and val3 is the 
+			//Piece of code that determines which if statement to use. 
 			string val1 = Console.ReadLine();
 			string val2 = Console.ReadLine();
 			string val3 = Console.ReadLine();
 			
 			double num1 = Convert.ToDouble(val1);
 			double num2 = Convert.ToDouble(val2);
-			
+       
+       //These if statements detemine weather the program will Add, Subtract, Multiply, or Divide. The will display the answer as an output.			
+       									
 			if (val3 == "Add")
 			{
 				double answer = num1 + num2;
